8a84b51f48af90001d68bc35d51f01ceec7c5086
marciorps <marciorps@yahoo.com,.br>
2023-11-23 13:51:38 -0300

    is_none

---
+++ oper_igual.py
@@ -0,0 +1,7 @@
+#Operadores de igualdade
+a = 'Python é demais! E ainda nem estamos no topo'
+b = 'Python é demais! E ainda nem estamos no topo'
+print(a == b)
+print(a is b)
+# == compara o valor
+# is compara a identidade
